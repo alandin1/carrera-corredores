2207cd02099e18610be75f14ec16b5bcb10af68d
alandin1 <alandin1@ucol.mx>
2020-10-08 14:45:38 -0500

    inicio java

---
+++ main.js
@@ -0,0 +1,19 @@
+class Caballo {
+    constructor(nombre = "Donald") {
+        this.nombre = nombre
+        this.avance = 0
+      }
+
+      turno() {
+        this.tiro = Math.ceil(Math.random()*6)
+         if (this.tiro==1) {
+          this.avance += 3
+          return this.avance
+        }
+        if (this.tiro0==2 || this.turno==3) {
+          this.avance += 1
+          return this.avance
+        }
+        if (this.tiro>3) {
+          this.avance 
+    
