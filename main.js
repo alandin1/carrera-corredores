eecde8f234107a3c83d70e8c545ab476a494103b
alandin1 <alandin1@ucol.mx>
2020-10-08 14:47:16 -0500

    clase caballo

--- main.js
+++ main.js
@@ -8,12 +8,20 @@ class Caballo {
         this.tiro = Math.ceil(Math.random()*6)
          if (this.tiro==1) {
           this.avance += 3
-          return this.avance
+            return this.avance
         }
+
         if (this.tiro0==2 || this.turno==3) {
           this.avance += 1
-          return this.avance
+            return this.avance
         }
+
         if (this.tiro>3) {
-          this.avance 
+          this.avance += 2
+             return this.avance
+        }
+        return this.avance += 0
+      }
+    }
+    
     
